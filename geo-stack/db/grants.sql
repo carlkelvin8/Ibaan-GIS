abb337b071196727dc09aac58cfe2a55fc54a445
SIddgrc <aliyah070206@gmail.com>
2026-02-07 21:13:25 +0800

    update

--- geo-stack/db/grants.sql
+++
@@ -1,5 +0,0 @@
--- db/grants.sql
--- Create a read-only user for GeoServer (adjust host as needed)
-CREATE USER IF NOT EXISTS 'gs'@'%' IDENTIFIED BY 'gs_password';
-GRANT SELECT ON gis.* TO 'gs'@'%';
-FLUSH PRIVILEGES;
